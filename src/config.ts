76ca9eeeba7daae03332450d0e268172ac9e821f
eerieA <ninja.duan@gmail.com>
2025-04-30 04:16:13 -0700

    readme and a few global constants

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,5 @@
 export const SITE_NAME = "Jane Doe";
 export const SITE_DESCRIPTION = "Personal portfolio of Jane Doe";
+export const LOCATION = "City, State";
+export const EMAIL = "your.name@domain.com";
+export const LINK1 = "github.com/username";
